ce8f8e3efb9262eb7c3cffc9bf7e192a38c4995a
Jihyeon An <jh011007@naver.com>
2025-11-21 16:27:11 +0900

    feat : controller API 응답 방식 변경

--- munglog-backend/src/main/java/com/example/munglogbackend/adapter/chat/ChatController.java
+++ munglog-backend/src/main/java/com/example/munglogbackend/adapter/chat/ChatController.java
@@ -5,6 +5,7 @@ import com.example.munglogbackend.application.chat.provided.ChatFinder;
 import com.example.munglogbackend.application.chat.provided.ChatSaver;
 import com.example.munglogbackend.domain.chat.dto.ChatMessageDto;
 import com.example.munglogbackend.domain.chat.dto.ChatRoomSummary;
+import com.example.munglogbackend.domain.global.apiPayload.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,43 +22,45 @@ public class ChatController {
 
     // 개인 채팅방 개설 또는 기존 roomId return
     @PostMapping("/private")
-    public Long createPrivateChatRoom(
+    public ApiResponse<Long> createPrivateChatRoom(
             @AuthenticationPrincipal AuthDetails authDetails,
             @RequestParam Long otherMemberId) {
-        return chatSaver.create(authDetails.getMemberId(), otherMemberId);
+        return ApiResponse.success(chatSaver.create(authDetails.getMemberId(), otherMemberId));
     }
 
     // 내 채팅방 목록 조회 : roomId, roomName, 그룹채팅여부, 메시지읽음개수
     @GetMapping("/rooms/me")
-    public List<ChatRoomSummary> getMyChatRooms(
+    public ApiResponse<List<ChatRoomSummary>> getMyChatRooms(
             @AuthenticationPrincipal AuthDetails authDetails) {
-        return chatFinder.findRoomsByMember(authDetails.getMemberId());
+        return ApiResponse.success(chatFinder.findRoomsByMember(authDetails.getMemberId()));
     }
 
     // 메시지 조회
     @GetMapping("/rooms/{roomId}/messages")
-    public Slice<ChatMessageDto> getMessages(
+    public ApiResponse<Slice<ChatMessageDto>> getMessages(
             @PathVariable Long roomId,
             @RequestParam(required = false) Long beforeSeq,
             @RequestParam(defaultValue = "50") int size,
             @AuthenticationPrincipal AuthDetails authDetails
     ) {
-        return chatFinder.fetchMessagesBeforeSeq(roomId, beforeSeq, size, authDetails.getMemberId());
+        return ApiResponse.success(chatFinder.fetchMessagesBeforeSeq(roomId, beforeSeq, size, authDetails.getMemberId()));
     }
 
     // 채팅 메시지 읽음 처리
     @PostMapping("/rooms/{roomId}/read")
-    public void markMessagesAsRead(
+    public ApiResponse<?> markMessagesAsRead(
             @AuthenticationPrincipal AuthDetails authDetails,
             @PathVariable Long roomId) {
         chatSaver.updateLastRead(roomId, authDetails.getMemberId());
+        return ApiResponse.success();
     }
 
     // 채팅방 나가기
     @DeleteMapping("/rooms/{roomId}")
-    public void leaveChatRoom(
+    public ApiResponse<?> leaveChatRoom(
             @AuthenticationPrincipal AuthDetails authDetails,
             @PathVariable Long roomId) {
         chatSaver.leaveChatRoom(roomId, authDetails.getMemberId());
+        return ApiResponse.success();
     }
 }
