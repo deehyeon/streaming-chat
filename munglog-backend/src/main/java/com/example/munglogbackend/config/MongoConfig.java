3cd7c579f5cc970af023a1703a47abf7f988a585
Jihyeon An <jh011007@naver.com>
2025-12-07 02:41:50 +0900

    feat : MongoConfig 생성

---
+++ munglog-backend/src/main/java/com/example/munglogbackend/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package com.example.munglogbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories(
+        basePackages = "com.example.munglogbackend.application.chat.required",
+        mongoTemplateRef = "mongoTemplate"
+)
+public class MongoConfig {
+}
