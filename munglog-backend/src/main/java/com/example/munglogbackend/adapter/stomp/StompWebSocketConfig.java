7424c077ca442a2790cc90b0bcd246d3bfa7057e
Jihyeon An <jh011007@naver.com>
2025-12-11 11:02:03 +0900

    feat : set allow origin

--- munglog-backend/src/main/java/com/example/munglogbackend/adapter/stomp/StompWebSocketConfig.java
+++ munglog-backend/src/main/java/com/example/munglogbackend/adapter/stomp/StompWebSocketConfig.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -40,8 +41,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOriginPatterns("*");
 
         registry.addEndpoint("/connect")
-                .setAllowedOrigins("http://localhost:3001")
-                .setAllowedOrigins("http://168.107.47.211")
+                .setAllowedOriginPatterns("*")
                 .withSockJS(); // ws://가 아닌 http:// 엔드포인트를 사용할 수 있게 해주는 sockJs 라이브러리를 통한 요청을 허용하는 설정
     }
 
@@ -69,5 +69,13 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(stompHandler);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration
+                .setMessageSizeLimit(512 * 1024)
+                .setSendBufferSizeLimit(1024 * 1024)
+                .setSendTimeLimit(20000);
     }
 }
