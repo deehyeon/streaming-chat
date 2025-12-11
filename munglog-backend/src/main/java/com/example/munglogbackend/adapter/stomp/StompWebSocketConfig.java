2a02de0161c8c65e49137bebe59f09848f4196a7
Jihyeon An <jh011007@naver.com>
2025-12-11 10:32:04 +0900

    feat : set allow origin

--- munglog-backend/src/main/java/com/example/munglogbackend/adapter/stomp/StompWebSocketConfig.java
+++ munglog-backend/src/main/java/com/example/munglogbackend/adapter/stomp/StompWebSocketConfig.java
@@ -41,6 +41,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         registry.addEndpoint("/connect")
                 .setAllowedOrigins("http://localhost:3001")
+                .setAllowedOrigins("http://168.107.47.211")
                 .withSockJS(); // ws://가 아닌 http:// 엔드포인트를 사용할 수 있게 해주는 sockJs 라이브러리를 통한 요청을 허용하는 설정
     }
 
